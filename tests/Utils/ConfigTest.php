4522020af26d1fe3624c293334add57287aba3a9
Clivern <hello@clivern.com>
2020-05-04 23:56:08 +0200

    update

---
+++ tests/Utils/ConfigTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Turtle project.
+ * (c) Clivern <hello@clivern.com>
+ */
+
+namespace App\Tests\Utils;
+
+use App\Utils\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testGet()
+    {
+        $this->assertSame((new Config())->get('APP_enV', null), 'test');
+        $this->assertSame((new Config())->get('app_env', null), 'test');
+        $this->assertSame((new Config())->get('not_Found', '~~not-found~~'), '~~not-found~~');
+    }
+}
