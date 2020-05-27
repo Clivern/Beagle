bc6c8ebda528f3132b77104c8b093e03650bbd5a
Clivern <hello@clivern.com>
2020-05-27 21:41:44 +0200

    init canonical-logger

---
+++ src/Utils/CanonicalLogger.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Beagle project.
+ * (c) Clivern <hello@clivern.com>
+ */
+
+namespace App\Utils;
+
+/**
+ * CanonicalLogger Utils.
+ *
+ * @see https://stripe.com/blog/canonical-log-lines
+ */
+class CanonicalLogger
+{
+}
